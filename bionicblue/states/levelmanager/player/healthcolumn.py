0dc5fb5b505621c882ea9bf0f7c2320c227a0b29
Kennedy Richard <kennedy@kennedyrichard.com>
2026-03-01 19:46:01 -0300

    underway: encounter and dialogue system
    TranslationNode only raises AttributeErrors now.

--- bionicblue/states/levelmanager/player/healthcolumn.py
+++ bionicblue/states/levelmanager/player/healthcolumn.py
@@ -5,6 +5,8 @@ from math import tau
 
 from itertools import cycle
 
+from collections import deque
+
 
 ### third-party imports
 
@@ -14,6 +16,7 @@ from pygame.draw import (
     arc as draw_arc,
     circle as draw_circle,
     line as draw_line,
+    polygon as draw_polygon,
     rect as draw_rect,
 )
 
@@ -30,6 +33,8 @@ from ....ourstdlibs.behaviour import do_nothing
 
 from ....ourstdlibs.pyl import load_pyl
 
+from ....pointsman2d.create import yield_circle_points
+
 
 
 ### XXX use variables to indicate the role of the named colors
@@ -44,18 +49,76 @@ NEXT_INCREASE_TWINKLING_COLOR = cycle(
     + (('brown',) * 15)
 ).__next__
 
+### star points
+
+OUTER_STAR_RADIUS = 8
+INNER_STAR_RADIUS = 4
+
+INNER_RADIUS_PROPORTION = INNER_STAR_RADIUS / OUTER_STAR_RADIUS
+
+NO_OF_STAR_TIPS = 5
+NO_OF_ROTATIONS = 6
+
+NO_OF_CIRCLE_POINTS = NO_OF_STAR_TIPS * NO_OF_ROTATIONS
+
+def _get_all_possible_rotated_star_points():
+
+    _circle_points_deque = deque(
+
+        yield_circle_points(
+            quantity=NO_OF_CIRCLE_POINTS,
+            radius=OUTER_STAR_RADIUS,
+        )
+
+    )
+
+    ###
+    points_of_each_rotated_star = deque()
+
+    center = Vector2(0, 0)
+
+    for _ in range(NO_OF_ROTATIONS):
+
+        star_points = []
+        append_point = star_points.append
+
+        star_tip = Vector2(_circle_points_deque[0])
+
+        for _ in range(NO_OF_STAR_TIPS):
+
+            _circle_points_deque.rotate(-NO_OF_ROTATIONS)
+
+            next_star_tip = Vector2(_circle_points_deque[0])
+
+            _midpoint = star_tip.lerp(next_star_tip, .5)
+            star_inner_point = center.lerp(_midpoint, INNER_RADIUS_PROPORTION)
+
+            append_point(star_inner_point)
+            append_point(next_star_tip)
+
+            star_tip = next_star_tip
+
+
+        points_of_each_rotated_star.append(star_points)
+
+        _circle_points_deque.rotate(-1)
+
+    _circle_points_deque.clear()
+
+    return points_of_each_rotated_star
+
+
+POINTS_OF_EACH_ROTATED_STAR = _get_all_possible_rotated_star_points()
+
+###
+ARC_OFFSET = tau * .35
+
+
 
 ### TODO
-### - use a star as the shape that moves on the spiral
-### - make that start rotate (with a dense set of points
-### representing a circle whose quantity is a multiple of
-### 5; this way you can put it in a deque and rotate it
-### to collect the equidistant subset of 5 points)
 ### - probably add a sound when star enters and another
 ### when the start touches the head (or perhaps just add
 ### this last one)
-### - make it so the green on the bar grows from below
-### rather than from above
 
 
 class HealthColumn:
@@ -102,6 +165,7 @@ class HealthColumn:
 
         self.inflated_bg_rect = hbg.inflate(4, 4)
         self.inflated_bg_rect_height = self.inflated_bg_rect.height
+        self.inflated_bg_rect_bottom = self.inflated_bg_rect.bottom
 
         draw_rect(image, 'grey80', self.inflated_bg_rect)
         draw_circle(image, 'grey80', head_rect.center, 7)
@@ -134,6 +198,10 @@ class HealthColumn:
             for point in load_pyl(MOTION_PATHS_DIR / 'star_spiral.pos')
 
         )
+
+        ### also create list for temporarily storing points of a star
+        ### to be drawn along the path
+        self.star_points = []
 
     def damage(self, amount):
 
@@ -180,7 +248,7 @@ class HealthColumn:
         self.bar_bg_percentage += 1
         image = self.image
 
-        if self.bar_bg_percentage < 100:
+        if self.bar_bg_percentage <= 100:
 
             image.fill(COLORKEY)
 
@@ -190,6 +258,7 @@ class HealthColumn:
 
             ihbg = self.inflated_bg_rect
             ihbg.height = self.inflated_bg_rect_height * unit_interval_percentage
+            ihbg.bottom = self.inflated_bg_rect_bottom
 
             draw_rect(image, 'springgreen', self.inflated_bg_rect)
 
@@ -197,13 +266,16 @@ class HealthColumn:
 
             draw_circle(image, 'springgreen', self.head_rect.center, 7)
 
+            start_angle = 0 + ARC_OFFSET
+            stop_angle = (tau * (1 - unit_interval_percentage)) + ARC_OFFSET
+
             draw_arc(
 
                 image,
                 COLORKEY,
                 self.inflated_head_rect,
-                0,                              # start angle
-                tau * (1 - unit_interval_percentage), # stop angle
+                start_angle,
+                stop_angle,
                 8,
 
             )
@@ -251,5 +323,24 @@ class HealthColumn:
 
     def spiral_draw(self):
 
+        ### draw health bar
         blit_on_screen(self.image, self.rect)
-        draw_circle(SCREEN, 'yellow', self.point, 5)
+
+        ### grab star points and perform other setups
+
+        offset = self.point
+        star_points = self.star_points
+
+        star_points.extend(point + offset for point in  POINTS_OF_EACH_ROTATED_STAR[0])
+        POINTS_OF_EACH_ROTATED_STAR.rotate(-1)
+
+        ### draw star
+
+        draw_polygon(
+            SCREEN,
+            'yellow',
+            star_points,
+        )
+
+        ### clear the list of star points so we can reuse it
+        star_points.clear()
